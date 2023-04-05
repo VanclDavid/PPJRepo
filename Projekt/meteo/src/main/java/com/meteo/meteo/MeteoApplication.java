e475005caeb9c872efedf324a91c77c7ba93080f
David Vancl <vancldada@gmail.com>
2023-04-05 17:02:34 +0200

    setup DB connection

--- Projekt/meteo/src/main/java/com/meteo/meteo/MeteoApplication.java
+++ Projekt/meteo/src/main/java/com/meteo/meteo/MeteoApplication.java
@@ -1,5 +1,9 @@
 package com.meteo.meteo;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +11,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MeteoApplication {
 
 	public static void main(String[] args) {
+		try {
+			Connection connection = DriverManager.getConnection(
+					"jdbc:mariadb://172.19.0.2:3306/meteo",
+					"root", "Aa123456");
+			PreparedStatement statement = connection.prepareStatement("""
+						CREATE TABLE CUSTOMERS(
+						ID INT NOT NULL,
+						NAME VARCHAR (20) NOT NULL,
+						AGE INT NOT NULL,
+						ADDRESS CHAR (25) ,
+						SALARY DECIMAL (18, 2),
+						PRIMARY KEY (ID)
+						)
+					""");
+			statement.executeQuery();
+			connection.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
 		SpringApplication.run(MeteoApplication.class, args);
 	}
 
--- Projekt/meteo/src/test/java/com/meteo/meteo/MeteoApplicationTests.java
+++ Projekt/meteo/src/test/java/com/meteo/meteo/MeteoApplicationTests.java
@@ -9,5 +9,4 @@ class MeteoApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
